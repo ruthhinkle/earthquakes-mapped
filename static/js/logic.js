6680fc2ae3243f6732e4dca4b04574b8b2a3a9c8
Ruth Hinkle <ruth.e.hinkle@gmail.com>
2021-04-15 17:12:04 -0500

    attempted to put data on map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,23 +1,41 @@
-// LINK TO DATA: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
+// IMPORT DATA & SET-UP MAP
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// Creating the map object
+
+// Create the map object
 var myMap = L.map("map", {
-    center: [40.7, -73.95],
-    zoom: 11
+    center: [28.304381,-196.526448],
+    zoom: 2
   });
   
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
-  // Store the API query variables.
-  // For docs, refer to https://dev.socrata.com/docs/queries/where.html.
-  // And, refer to https://dev.socrata.com/foundry/data.cityofnewyork.us/erm2-nwe9.
-  var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+// Adding the tile layer for Open Street Map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Definite URL variable of geoJSON link
+var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson?";
 
 // Import & Visualize the Data
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+d3.json(baseURL, function(data) {
+  console.log(data)
+  createImageBitmap(data.features)
+  // console.log(magnitude)
+});
+
+
+// Store the API query variables.
+var quake_features = data.features
+var magnitude = '$where=magnitude
+var depth = data.geometry.coordinates[2]
+console.log(depth)
+
+
+// For docs, refer to https://dev.socrata.com/docs/queries/where.html.
+// And, refer to https://dev.socrata.com/foundry/data.cityofnewyork.us/erm2-nwe9.
+
 
 // Create a map using Leaflet that plots all of the earthquakes from your data set based on their longitude and latitude.
 
